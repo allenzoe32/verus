0c4c6b723c7507e4b21af6ed4fc4fe4d61152320
allenzoe32 <95068651+allenzoe32@users.noreply.github.com>
2024-08-19 16:03:47 +0700

    Add files via upload

---
+++ run_miner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# For more usage info: hellminer --help
+
+# Example SSL URL
+# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RAJgZ9tnAF5KuU1A42rMsp7Mt2KNtMQGDB.QH001 -p hybrid --cpu 6
